e227719af875e03692b5b371c76d62ff83597386
finnfinnchen <finnchen26@gmail.com>
2019-02-10 19:03:06 +0100

    Revert "put"
    This reverts commit 8b9fd34787f3c5523d9d3eb56fabccf89d276660.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // land and water use
 $("#btn1").click(function(event){
   $("#btext1").css("fill","#FFFFFF");
-  $("#bottn1").css("fill","#000000");
+  $("#btn1").css("fill","#000000");
   $("#btext2").css('fill','#000000');
-  $("#bottn2").css('fill','none','stroke','#3C3C3B');
+  $("#btn2").css('fill','none','stroke','#3C3C3B');
   if($('#water').css("display") == 'none'){
     $("#soil").show();
   }
