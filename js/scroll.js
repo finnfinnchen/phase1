48e583571209079d3618d7520260265603047d60
finnfinnchen <finnchen26@gmail.com>
2019-02-10 18:51:42 +0100

    Revert "Merge branch 'master' of https://github.com/finnfinnchen/phase1"
    This reverts commit 50189c7ffe70d9d5a5a341f2290e7d299f8ac00b, reversing
    changes made to 8ad560932fc5b9b2a09e612e893859dc23d92f44.

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         //options here
-        anchors: ['firstPage','secondPage','thirdPage','4thPage','5thPage'],
+        anchors: ['firstPage','secondPage','thirdPage'],
         navigation: true,
         navigationPosition: 'right',
         scrollingSpeed: 700,
         css3: true,
         autoScrolling: true,
         scrollOverflow: false,
-        sectionsColor : ['#cbce89','#cbce89','#cbce89','#a6bf6c','#a6bf6c','#a6bf6c','#04151F'],
+        sectionsColor : ['#a6bf6c','#a6bf6c','#04151F'],
         navigationTooltips : ['Chile water scarcity','Production growth'],
     });
 
