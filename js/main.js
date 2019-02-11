4a264cece273709d869207206efd6657e36e5a66
wu bin <532787286@qq.com>
2019-02-11 11:09:37 +0100

    2.11

--- js/main.js
+++ js/main.js
@@ -1,22 +1,28 @@
 // land and water use
 $("#btn1").click(function(event){
   $("#btext1").css("fill","#FFFFFF");
-  $("#bottn1").css("fill","#FFFFFF");
+  $("#bottn1").css("fill","#000000");
   $("#btext2").css('fill','#000000');
   $("#bottn2").css('fill','none','stroke','#3C3C3B');
   if($('#water').css("display") == 'none'){
-    $("#soil").show();
+    $("#land").show();
   }
   else{
     $("#water").hide();
-    $("#soil").show();
+    $("#land").show();
   }
 })
 
-// water footprint
-$("#bnt4").mouseenter(function(event){
-  $("#grey").show();  
-    $("#bnt4").mouseleave(function(event){
-  $("#grey").hide();  
-    
+$("#btn2").click(function(event){
+  $("#btext2").css("fill","#FFFFFF");
+  $("#bottn2").css("fill","#000000");
+  $("#btext1").css('fill','#000000');
+  $("#bottn1").css('fill','none','stroke','#3C3C3B');
+  if($('#land').css("display") == 'none'){
+    $("#water").show();
+  }
+  else{
+    $("#land").hide();
+    $("#water").show();
+  }
 })
