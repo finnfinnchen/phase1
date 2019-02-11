988b6fd6a8c71b8f9bc1e13d77264a66eb50caee
wu bin <532787286@qq.com>
2019-02-11 19:59:36 +0100

    begining part

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-// land and water use
-$("#btn1").click(function(event){
-  $("#btext1").css("fill","#FFFFFF");
-  $("#bottn1").css("fill","#000000");
-  $("#btext2").css('fill','#000000');
+// the use of resource
+$("#soil_btn").click(function(event){
+  $("#s_text").css("fill","#FFFFFF");
+  $("#s_rec").css("fill","#000000");
+  $("#w_text").css('fill','#000000');
+  $("#water_btn").css('fill','none','stroke','#3C3C3B');
+  if($('#water_consumption').css("display") == 'none'){
+    $("#soil_consumption").show();
+  }
+  else{
+    $("#water_consumption").hide();
+    $("#soil_consumption").show();
+  }
+$("#water_btn").click(function(event){
+  $("#w_text").css("fill","#FFFFFF");
+  $("#w_rec").css("fill","#000000");
+  $("#s_text").css('fill','#000000');
+  $("#soil_btn").css('fill','none','stroke','#3C3C3B');
+  if($('#soil_consumption').css("display") == 'none'){
+    $("#water_consumption").show();
+  }
+  else{
+    $("#soil_consumption").hide();
+    $("#water_consumption").show();
+  }
+
+// land and water comparison
+$("#btn1").click(function (event) {
+  $("#btext1").css("fill", "#FFFFFF");
+  $("#bottn1").css("fill", "#000000");
+  $("#btext2").css('fill', '#000000');
   $("#bottn2").css('fill','none','stroke','#3C3C3B');
   if($('#water').css("display") == 'none'){
     $("#land").show();
@@ -13,10 +39,10 @@ $("#btn1").click(function(event){
   }
 })
 
-$("#btn2").click(function(event){
-  $("#btext2").css("fill","#FFFFFF");
-  $("#bottn2").css("fill","#000000");
-  $("#btext1").css('fill','#000000');
+$("#btn2").click(function (event) {
+  $("#btext2").css("fill", "#FFFFFF");
+  $("#bottn2").css("fill", "#000000");
+  $("#btext1").css('fill', '#000000');
   $("#bottn1").css('fill','none','stroke','#3C3C3B');
   if($('#land').css("display") == 'none'){
     $("#water").show();
@@ -25,4 +51,4 @@ $("#btn2").click(function(event){
     $("#land").hide();
     $("#water").show();
   }
-})
+}) 
